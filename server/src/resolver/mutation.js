ae7d62cdbec80c341ac3a22184c4383ffeeef5f7
nutchapon.h <nutchapon@enres.com>
2020-05-24 20:07:27 +0700

    day 4 - add mutation update/delete post

--- server/src/resolver/mutation.js
+++ server/src/resolver/mutation.js
@@ -9,6 +9,50 @@ const Mutation = {
       info
     );
   },
+  async updatePost(parent, args, { prisma }, info) {
+    const postId = args.id;
+    let opArgs = {};
+
+    const postExits = await prisma.exists.Post({
+      id: postId,
+    });
+
+    console.log(postExits);
+
+    if (!postExits) {
+      throw new Error("post not found");
+    }
+
+    opArgs = {
+      where: {
+        id: postId,
+      },
+      data: {
+        ...args.data,
+      },
+    };
+
+    return prisma.mutation.updatePost(opArgs, info);
+  },
+  async deletePost(parent, args, { prisma }, info) {
+    const postId = args.id;
+
+    const postExits = await prisma.exists.Post({
+      id: postId,
+    });
+
+    if (!postExits) {
+      throw new Error("post not found");
+    }
+
+    const opArgs = {
+      where: {
+        id: postId,
+      },
+    };
+
+    return prisma.mutation.deletePost(opArgs, info);
+  },
 };
 
 export { Mutation as default };
