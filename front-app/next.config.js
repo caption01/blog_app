a2d6644659d3aefe5b7a22a35205cdee11cfa8f8
nutchapon.h <nutchapon@enres.com>
2020-06-03 20:59:13 +0700

    day 11 - change front-app docker file and first deploy to hosting

--- front-app/next.config.js
+++ front-app/next.config.js
@@ -20,4 +20,28 @@ module.exports = withLess({
     javascriptEnabled: true,
     modifyVars: themeVariables,
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/;
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === "function") {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === "function" ? [] : origExternals),
+      ];
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: "null-loader",
+      });
+    }
+
+    return config;
+  },
 });
--- front-app/utils/apollo.js
+++ front-app/utils/apollo.js
@@ -4,11 +4,13 @@ import withApollo from "next-with-apollo";
 import { createHttpLink } from "apollo-link-http";
 import fetch from "isomorphic-unfetch";
 
-const GRAPHQL_URL = "http://localhost:4010";
+// const GRAPHQL_URL = "http://localhost:4010";
+
+const API_GRAPHQL = process.env.NEXT_PUBLIC_API_GRAPHQL;
 
 const link = createHttpLink({
   fetch,
-  uri: GRAPHQL_URL,
+  uri: API_GRAPHQL,
 });
 
 export default withApollo(
