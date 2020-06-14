99079a770d75b5e703de6f5c4c782589c9425a2b
nutchapon.h <nutchapon@enres.com>
2020-06-14 15:45:51 +0700

    apollo hook problem

--- front-app/component/layout/topNav.js
+++ front-app/component/layout/topNav.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useMutation } from "@apollo/react-hooks";
 import styled from "styled-components";
-import { Layout } from "antd";
+import { Layout, Drawer, Menu, Tooltip } from "antd";
 import { withTheme } from "styled-components";
+import { MenuOutlined } from "@ant-design/icons";
 
 import SocialContactWebView from "./SocialContactWeb";
 import SocialContactMobileView from "./SocialContactMobile";
+import SearchBar from "../searchBar/searchBar";
 import { listContact } from "./listContactData";
+
+import { UPDATE_SEARCH } from "../../utils/mutation/index";
 
 const { Header } = Layout;
 
+//mainTopNav - styled
 const StyledTopNav = styled(Header)`
   background-color: ${(props) => props.theme.colors.bg_primary};
   color: ${(props) => props.theme.colors.text_primary};
@@ -42,8 +48,109 @@ const SocialContact = styled.div`
   align-items: center;
 `;
 
+//webView - styled
+const IconDiv = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0px 20px;
+
+  a {
+    color: inherit;
+    line-height: ${(props) => (props.theme.isMobile ? "20px" : "40px")};
+    font-size: ${(props) => (props.theme.isMobile ? "20px" : "40px")};
+  }
+
+  :hover {
+    cursor: pointer;
+    -webkit-box-shadow: 0px 0px 29px 19px rgba(0, 0, 0, 0.52);
+    -moz-box-shadow: 0px 0px 29px 19px rgba(0, 0, 0, 0.52);
+    box-shadow: 0px 0px 29px 19px rgba(0, 0, 0, 0.52);
+  }
+`;
+
+const DivMarginRight = styled.div`
+  margin: 0px 20px;
+`;
+
+//moblieBiew - styled
+const StyledButton = styled.div`
+  background-color: ${(props) => props.theme.colors.bg_primary};
+  font-size: 20px;
+`;
+
+const renderContactWeb = (listContact, callback) => {
+  return (
+    <>
+      <DivMarginRight>
+        <SearchBar onSearch={(value) => callback(value)} />
+      </DivMarginRight>
+      {listContact.map((contact) => {
+        return (
+          <Tooltip title={contact.title} key={contact.key}>
+            <IconDiv>
+              <a target="_blank" href={contact.url}>
+                {contact.icon}
+              </a>
+            </IconDiv>
+          </Tooltip>
+        );
+      })}
+    </>
+  );
+};
+
+const renderContactMoblie = (listContact, callback) => {
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  return (
+    <>
+      <StyledButton type="primary" onClick={() => showDrawer()}>
+        <MenuOutlined />
+      </StyledButton>
+      <Drawer
+        title="StuffStick"
+        placement="right"
+        closable={false}
+        onClose={() => onClose()}
+        visible={visible}
+      >
+        <DivMarginRight>
+          <SearchBar onSearch={(value) => callback(value)} />
+        </DivMarginRight>
+        <Menu style={{ width: 256, marginTop: "20px" }} mode="inline">
+          {listContact.map((contact) => {
+            return (
+              <Menu.Item key={contact.key}>
+                {contact.icon}
+                <a target="_blank" href={contact.url}>
+                  {contact.title}
+                </a>
+              </Menu.Item>
+            );
+          })}
+        </Menu>
+      </Drawer>
+    </>
+  );
+};
+
 const TopNav = ({ theme }) => {
   const isMobileView = theme?.isMobile;
+  const [setSearch] = useMutation(UPDATE_SEARCH);
+
+  const handleSearchChange = (value) => {
+    setSearch({
+      variables: { search: value },
+    });
+  };
 
   return (
     <StyledTopNav>
@@ -51,11 +158,9 @@ const TopNav = ({ theme }) => {
         <img src={"./static/stuff_logo_2.png"} alt="logo" />
       </LogoFrame>
       <SocialContact>
-        {!isMobileView ? (
-          <SocialContactWebView listContact={listContact} />
-        ) : (
-          <SocialContactMobileView listContact={listContact} />
-        )}
+        {isMobileView
+          ? renderContactMoblie(listContact, handleSearchChange)
+          : renderContactWeb(listContact, handleSearchChange)}
       </SocialContact>
     </StyledTopNav>
   );
